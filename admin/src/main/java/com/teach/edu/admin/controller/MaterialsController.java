06e49047952208d1e57c15c44f4440804064dfdd
lhs <tsxylhs@qq.com>
2020-04-01 22:12:41 +0800

    fix

--- admin/src/main/java/com/teach/edu/admin/controller/MaterialsController.java
+++ admin/src/main/java/com/teach/edu/admin/controller/MaterialsController.java
@@ -13,9 +13,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.IdGenerator;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.activation.MimetypesFileTypeMap;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.Random;
 import java.util.UUID;
@@ -83,6 +88,7 @@ public class MaterialsController {
         materialsService.add(matterials);
         return Result.ok();
     }
+
     @DeleteMapping("/{id}")
     @ApiOperation(value = "删除资料")
     @ApiImplicitParam(paramType = "query", name = "id", value = "用户id", required = true, dataType = "long")
@@ -95,6 +101,37 @@ public class MaterialsController {
     @ApiOperation(value = "获取作业列表")
     public Result userList(@RequestBody PageRequest pageRequest) {
         return materialsService.list();
+    }
+
+    @GetMapping("/download/{id}")
+    @ApiOperation(value = "下载文件")
+    public void download(@PathVariable Long id)throws IOException {
+       Materials materials= materialsService.get(id);
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = requestAttributes.getResponse();
+        // 设置信息给客户端不解析
+        String type = new MimetypesFileTypeMap().getContentType(materials.getMaterialsDesc());
+        // 设置contenttype，即告诉客户端所发送的数据属于什么类型
+        response.setHeader("Content-type","application/msword");
+        // 设置编码
+        String hehe = new String(materials.getMaterialsDesc().getBytes("utf-8"), "iso-8859-1");
+        // 设置扩展头，当Content-Type 的类型为要下载的类型时 , 这个信息头会告诉浏览器这个文件的名字和类型。
+        response.setHeader("Content-Disposition", "attachment;filename=" + hehe);
+
+        // 发送给客户端的数据
+        OutputStream outputStream = response.getOutputStream();
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        // 读取filename
+        bis = new BufferedInputStream(new FileInputStream(new File("./tmp/"+materials.getMaterialsDesc())));
+        int i = bis.read(buff);
+        while (i != -1) {
+            outputStream.write(buff, 0, buff.length);
+            outputStream.flush();
+            i = bis.read(buff);
+        }
+
+
     }
 
     private void savePic(InputStream inputStream, String fileName) {
